7ab24cb3879217c31113a9ea2dade089fbe9c1be
Ferdy <93808738+verdia20@users.noreply.github.com>
2022-06-01 19:36:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -23,34 +23,7 @@ class Game:
         self.cloud = pygame.sprite.Group()
         self.item = pygame.sprite.Group()
 
-        # audio setup
-        self.bg_music = pygame.mixer.Sound('audio/airship.flac')
-        self.bg_music.set_volume(0.5)
-        self.intro_music = pygame.mixer.Sound('audio/happy.mp3')
-        self.intro_music.set_volume(0.5)
-        self.game_over_music = pygame.mixer.Sound('audio/sunshine.mp3')
-        self.game_over_music.set_volume(0.5)
-        self.coin_sound = pygame.mixer.Sound('audio/coin.mp3')
-        self.coin_sound.set_volume(0.5)
-        self.explosion_sound = pygame.mixer.Sound('audio/explosion.flac')
-        self.explosion_sound.set_volume(0.5)
-        self.dead_sound = pygame.mixer.Sound('audio/dead.flac')
-        self.dead_sound.set_volume(0.5)
-        self.item_sound = pygame.mixer.Sound('audio/item.mp3')
-        self.item_sound.set_volume(0.5)
-        self.wind_sound = pygame.mixer.Sound('audio/wind.wav')
-        self.wind_sound.set_volume(0.5)
-
-        # game additonal setup
-        self.bg_surface = pygame.image.load('graphics/background.png').convert_alpha()
-        self.bg_surface = pygame.transform.smoothscale(self.bg_surface, (SCREEN_WIDTH, SCREEN_HEIGHT))
-        self.intro_bg = pygame.image.load('graphics/intro_bg.png').convert_alpha()
-        self.intro_bg = pygame.transform.smoothscale(self.intro_bg, (SCREEN_WIDTH, SCREEN_HEIGHT))
-        self.game_over_bg = pygame.image.load('graphics/game_over_bg.png').convert_alpha()
-        self.game_over_bg = pygame.transform.smoothscale(self.game_over_bg, (SCREEN_WIDTH, SCREEN_HEIGHT))
-        self.indicator = pygame.image.load('graphics/player/intro.png').convert_alpha()
-        self.indicator = pygame.transform.smoothscale(self.indicator, (55, 37))
-        self.indicator_rect = self.indicator.get_rect(center = (40, 30))
+        # game condition and score
         self.game_active = False
         self.score = 0
         self.high_score = 0
@@ -76,7 +49,7 @@ class Game:
     def collision_player(self):
         # jika terjadi collision maka akan mengosongkan semua grup objek
         if pygame.sprite.spritecollide(self.player.sprite, self.misil, False):
-            self.dead_sound.play()
+            dead_sound.play()
             self.misil.empty()
             self.coin.empty()
             self.cloud.empty()
@@ -92,7 +65,7 @@ class Game:
         # jika terjadi collision maka akan akan menghapus objek bullet dari group
         for bullet in self.player.sprite.bullets:
             if pygame.sprite.spritecollide(bullet, self.misil, True):
-                self.explosion_sound.play()
+                explosion_sound.play()
                 bullet.kill()
                 return True
         return False
@@ -100,7 +73,7 @@ class Game:
     # menampilkan indicator ability player di dalam game
     def item_indicator(self):
         if self.player.sprite.indicator_active:
-            screen.blit(self.indicator, self.indicator_rect)
+            screen.blit(indicator, indicator_rect)
 
     def cheat(self):
         keys = pygame.key.get_pressed()
@@ -108,7 +81,7 @@ class Game:
             self.score += 1
         if keys[pygame.K_o]:
             self.game_active = False
-            self.dead_sound.play()
+            dead_sound.play()
             self.misil.empty()
             self.coin.empty()
             self.cloud.empty()
@@ -116,10 +89,10 @@ class Game:
             self.player.sprite.bullets.empty()
             self.player.sprite.bullet_active = False
 
-    # main program
-    def main(self):
-        self.bg_music.play(-1)
-        self.wind_sound.play(-1)
+    # main game
+    def main_game(self):
+        bg_music.play(-1)
+        wind_sound.play(-1)
         # game loop
         while True:
             for event in pygame.event.get():
@@ -138,18 +111,18 @@ class Game:
                         self.item.add(Item())
 
             if self.game_active:
-                screen.blit(self.bg_surface, (0, 0))
+                screen.blit(bg_surface, (0, 0))
                 self.high_score_check()
                 coin_hit = pygame.sprite.spritecollide(self.player.sprite, self.coin, True)
                 # jika terjadi coin_hit maka akan menambahkan score
                 if coin_hit:
-                    self.coin_sound.play()
+                    coin_sound.play()
                     self.score+=1
 
                 item_hit = pygame.sprite.spritecollide(self.player.sprite, self.item, True)
                 # mengaktifkan kemampuan menembak player
                 if item_hit:
-                    self.item_sound.play()
+                    item_sound.play()
                     self.player.sprite.bullet_active = True
                     self.player.sprite.active_time = pygame.time.get_ticks()
                 
@@ -182,8 +155,8 @@ class Game:
                     with open('high_score.txt', 'w') as file:
                         file.write(str(self.high_score))
 
-                self.bg_music.stop()
-                self.wind_sound.stop()
+                bg_music.stop()
+                wind_sound.stop()
                 self.game_over()
 
             pygame.display.update()
@@ -191,7 +164,7 @@ class Game:
 
     # menampilkan game over screen
     def game_over(self):
-        self.game_over_music.play()
+        game_over_music.play()
         # tampilan untuk game over
         game_over_message = self.get_font(40).render('Game Over', True, (203,19,13))
         game_over_message_rect = game_over_message.get_rect(center = ((SCREEN_WIDTH/2), 100))
@@ -217,16 +190,16 @@ class Game:
                     if event.key == pygame.K_SPACE:
                         run = False
                         self.game_active = True
-                        self.game_over_music.stop()
+                        game_over_music.stop()
                         self.score = 0
-                        self.main()
+                        self.main_game()
                     if event.key == pygame.K_ESCAPE:
                         run = False
-                        self.game_over_music.stop()
+                        game_over_music.stop()
                         self.main_menu()
                         
             # untuk menampilkan ke layar
-            screen.blit(self.game_over_bg, (0, 0))
+            screen.blit(game_over_bg, (0, 0))
             screen.blit(game_over_message, game_over_message_rect)
             screen.blit(score_massage, score_massage_rect)
             screen.blit(high_score_message, high_score_message_rect)
@@ -238,7 +211,7 @@ class Game:
 
     # menampilkan main menu
     def main_menu(self):
-        self.intro_music.play(-1)
+        intro_music.play(-1)
         # tampilan untuk main menu
         title_text = self.get_font(35).render('Aircraft: Fly Forever', True, ('#cb130d'))
         title_rect = title_text.get_rect(center = (400, 30))
@@ -266,7 +239,7 @@ class Game:
                                 text_input="QUIT", font=self.get_font(35), base_color="Black", hovering_color="#baf4fc")
 
             # menampilkan ke layar
-            screen.blit(self.intro_bg, (0, 0))
+            screen.blit(intro_bg, (0, 0))
             screen.blit(title_text, title_rect)
             screen.blit(player_stand, player_stand_rect)
             screen.blit(menu_text, menu_rect)
@@ -287,9 +260,9 @@ class Game:
                     # untuk mengecek apakah tombol diklik
                     if play_button.check_for_input(menu_mouse_pos):
                         run = False
-                        self.intro_music.stop()
+                        intro_music.stop()
                         self.game_active = True
-                        self.main()
+                        self.main_game()
                     if quit_button.check_for_input(menu_mouse_pos):
                         pygame.quit()
                         exit()
@@ -315,6 +288,35 @@ cloud_timer = pygame.USEREVENT + 3
 pygame.time.set_timer(cloud_timer, randint(1500, 2000))
 item_timer = pygame.USEREVENT + 4
 pygame.time.set_timer(item_timer, randint(8000, 10000))
+
+# game additonal setup
+bg_surface = pygame.image.load('graphics/background.png').convert_alpha()
+bg_surface = pygame.transform.smoothscale(bg_surface, (SCREEN_WIDTH, SCREEN_HEIGHT))
+intro_bg = pygame.image.load('graphics/intro_bg.png').convert_alpha()
+intro_bg = pygame.transform.smoothscale(intro_bg, (SCREEN_WIDTH, SCREEN_HEIGHT))
+game_over_bg = pygame.image.load('graphics/game_over_bg.png').convert_alpha()
+game_over_bg = pygame.transform.smoothscale(game_over_bg, (SCREEN_WIDTH, SCREEN_HEIGHT))
+indicator = pygame.image.load('graphics/player/intro.png').convert_alpha()
+indicator = pygame.transform.smoothscale(indicator, (55, 37))
+indicator_rect = indicator.get_rect(center = (40, 30))
+
+# audio setup
+bg_music = pygame.mixer.Sound('audio/airship.flac')
+bg_music.set_volume(0.5)
+intro_music = pygame.mixer.Sound('audio/happy.mp3')
+intro_music.set_volume(0.5)
+game_over_music = pygame.mixer.Sound('audio/sunshine.mp3')
+game_over_music.set_volume(0.5)
+coin_sound = pygame.mixer.Sound('audio/coin.mp3')
+coin_sound.set_volume(0.5)
+explosion_sound = pygame.mixer.Sound('audio/explosion.flac')
+explosion_sound.set_volume(0.5)
+dead_sound = pygame.mixer.Sound('audio/dead.flac')
+dead_sound.set_volume(0.5)
+item_sound = pygame.mixer.Sound('audio/item.mp3')
+item_sound.set_volume(0.5)
+wind_sound = pygame.mixer.Sound('audio/wind.wav')
+wind_sound.set_volume(0.5)
 
 # game run
 game = Game()
